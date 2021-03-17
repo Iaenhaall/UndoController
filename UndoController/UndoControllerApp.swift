a6bb7d902aff3faa65701e29043c659bd6f0d94c
Никита Белокриницкий <iaenhaall@ya.ru>
2021-03-17 22:51:12 +0500

    Project creation.

---
+++ UndoController/UndoControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndoControllerApp.swift
+//  UndoController
+//
+//  Created by Никита Белокриницкий on 17.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct UndoControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
